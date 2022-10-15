5b6294d1ec31b4d8f551a2613e2eed8b2517d577
Enes Karakas <enes.coding@gmail.com>
2022-10-15 12:07:49 +0300

    section4done, wathes and bands

--- src/script.js
+++ src/script.js
@@ -1,3 +1,12 @@
+//prevent default for all controls
+document
+  .querySelectorAll('.watch-control, .controls a, .iphone-btn')
+  .forEach((control) => {
+    control.addEventListener('click', (e) => {
+      e.preventDefault();
+    });
+  });
+
 //global VARS
 const cube = document.querySelector('.cube');
 let y = 20;
@@ -107,9 +116,67 @@ function macBook() {
   });
 }
 
+// section4
+
+const watchBands = document.querySelector('.watch-bands');
+const watchCases = document.querySelector('.watch-cases');
+
+const watchTopControl = document.querySelector('.watch-top-control');
+const watchRightControl = document.querySelector('.watch-right-control');
+const watchBottomControl = document.querySelector('.watch-bottom-control');
+const watchLeftControl = document.querySelector('.watch-left-control');
+
+let axisY = 0;
+let axisX = 0;
+
+const hideControl = () => {
+  if (axisY === -280) {
+    watchTopControl.classList.add('hideControl');
+  } else {
+    watchTopControl.classList.remove('hideControl');
+  }
+
+  if (axisY === 280) {
+    watchBottomControl.classList.add('hideControl');
+  } else {
+    watchBottomControl.classList.remove('hideControl');
+  }
+
+  if (axisX === 280) {
+    watchRightControl.classList.add('hideControl');
+  } else {
+    watchRightControl.classList.remove('hideControl');
+  }
+
+  if (axisX === -280) {
+    watchLeftControl.classList.add('hideControl');
+  } else {
+    watchLeftControl.classList.remove('hideControl');
+  }
+};
+
+watchTopControl.addEventListener('click', () => {
+  watchCases.style.marginTop = `${(axisY -= 70)}rem`;
+  hideControl();
+});
+
+watchBottomControl.addEventListener('click', () => {
+  watchCases.style.marginTop = `${(axisY += 70)}rem`;
+  hideControl();
+});
+
+watchRightControl.addEventListener('click', () => {
+  watchBands.style.marginRight = `${(axisX += 70)}rem`;
+  hideControl();
+});
+
+watchLeftControl.addEventListener('click', () => {
+  watchBands.style.marginRight = `${(axisX -= 70)}rem`;
+  hideControl();
+});
+
 // slideShowDivs();
 // infiniteBackgroundSlide();
-
 // allControls();
 // playPauseCube();
 // hoverOnCube();
